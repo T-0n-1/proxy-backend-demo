08574405cfb49d8173c76e0a9291e9579c7674f4
Toni Mertanen <t.mertanen@hotmail.com>
2024-11-19 22:03:30 +0200

    Backend server build starting

--- src/backend.ts
+++ src/backend.ts
@@ -1,7 +1,16 @@
-import express from "express";
+import express, { Express } from "express";
 import path from "path";
-import { PathOrFileDescriptor } from "fs";
+import { PathOrFileDescriptor }, fs from "fs";
 import dotenv from "dotenv";
 import type { Student } from "./Interfaces";
 
 dotenv.config();
+
+const app: Express = express();
+const port = Number(process.env.BACKPORT) || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Backend server running on port ${port}`);
+});
