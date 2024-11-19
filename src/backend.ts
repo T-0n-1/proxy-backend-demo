de61be6c9e81a8dda6f009f46a182500b340fec5
Toni Mertanen <t.mertanen@hotmail.com>
2024-11-19 21:58:26 +0200

    Add data into JSON object

---
+++ src/backend.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import path from "path";
+import { PathOrFileDescriptor } from "fs";
+import dotenv from "dotenv";
+import type { Student } from "./Interfaces";
+
+dotenv.config();
